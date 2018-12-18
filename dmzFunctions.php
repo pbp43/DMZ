4cbbdc3972e350e872206cddd95dc694c4c12e5f
pbp43 <35785947+pbp43@users.noreply.github.com>
2018-12-18 17:45:34 -0500

    final files

--- dmzFunctions.php
+++ dmzFunctions.php
@@ -1,8 +1,8 @@
 <?php
   
-  	require_once('path.inc');
-    	require_once('get_host_info.inc');
-    	require_once('rabbitMQLib.inc');
+  	require_once('../RabbitMQ/path.inc');
+    	require_once('../RabbitMQ/get_host_info.inc');
+    	require_once('../RabbitMQ/rabbitMQLib.inc');
 	//require_once('testApiSag.php');
 
 
@@ -44,7 +44,89 @@
 		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://api.nutritionix.com/v1_1/search/'.$item.'?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_sodium&appId=aac181c3&appKey=18d1adac27be37b70e42974393087394",
+		CURLOPT_URL => "https://api.nutritionix.com/v1_1/search/'.$item.'?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_sodium,nf_sugars,nf_protein,nf_totalcarbohydrate&appId=aac181c3&appKey=18d1adac27be37b70e42974393087394",
+  		CURLOPT_RETURNTRANSFER => true,
+  		CURLOPT_ENCODING => "",
+  		CURLOPT_MAXREDIRS => 10,
+  		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  		CURLOPT_CUSTOMREQUEST => "GET",
+  		CURLOPT_HTTPHEADER => array(
+    		"Postman-Token: eb158d0e-a543-4895-8d47-e4e0f967be96",
+    		"cache-control: no-cache"
+  		),
+));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+  			echo "cURL Error #:" . $err;
+		} else {
+  			echo $response;
+			
+
+		$result = json_decode($response, true);
+		//print_r($result);
+		$hits = $result['hits'];
+		$item_info = $hits['0'];
+		print_r($item_info);
+		return $item_info;
+	}
+    
+  }
+
+
+	function calorietrack($item,$username){		
+   
+   
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.nutritionix.com/v1_1/search/'.$item.'?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_sodium,nf_sugars,nf_protein,nf_totalcarbohydrate&appId=aac181c3&appKey=18d1adac27be37b70e42974393087394",
+  		CURLOPT_RETURNTRANSFER => true,
+  		CURLOPT_ENCODING => "",
+  		CURLOPT_MAXREDIRS => 10,
+  		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  		CURLOPT_CUSTOMREQUEST => "GET",
+  		CURLOPT_HTTPHEADER => array(
+    		"Postman-Token: eb158d0e-a543-4895-8d47-e4e0f967be96",
+    		"cache-control: no-cache"
+  		),
+));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+  			echo "cURL Error #:" . $err;
+		} else {
+  			echo $response;
+			
+
+		$result = json_decode($response, true);
+		//print_r($result);
+		$hits = $result['hits'];
+		$item_info = $hits['0'];
+		print_r($item_info);
+		return $item_info;
+	}
+    
+  }
+	function attributesearch($item,$username){		
+   
+   
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.nutritionix.com/v1_1/search/'.$item.'?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_sodium,nf_sugars,nf_protein,nf_totalcarbohydrate&appId=aac181c3&appKey=18d1adac27be37b70e42974393087394",
   		CURLOPT_RETURNTRANSFER => true,
   		CURLOPT_ENCODING => "",
   		CURLOPT_MAXREDIRS => 10,
@@ -91,7 +173,16 @@
 	    case "search":
                 return fetchItem($request['item']);
                 break;
-            
+	
+	    case "calorietrack":
+		return calorietrack($request['item'], $request['username']);
+		break;
+	    
+	    case "attributesearch":
+		return attributesearch($request['item'], $request['username']);
+		break;
+		
+             
         }
        	echo var_dump($response_msg);
         return $response_msg;
@@ -105,6 +196,6 @@
     $server->process_requests('requestProcessor');
 
 	exit();
-    
+
 
 ?>
--- testApiSag.php
+++ testApiSag.php
@@ -3,7 +3,7 @@
 
 	ini_set("allow_url_fopen", 1);
 
-	$url = "https://api.nutritionix.com/v1_1/search/banana?			results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=e2776803&appKey=bb63a372b37ecad9e72e41699ba3f239";
+	$url = "https://api.nutritionix.com/v1_1/search/banana?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=e2776803&appKey=bb63a372b37ecad9e72e41699ba3f239";
 	//echo $url;
 	
 	//echo $json;
